12db91225b92be917dfb1de0d6b30c168a205624
Chenghao <hcheng50@best-inc.com>
2019-03-28 11:22:11 +0800

    @PostConstruct

--- src/main/java/com/ncu/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/ncu/springboot/service/impl/UserServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
 public class UserServiceImpl implements UserService {
@@ -16,6 +18,19 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
     @Autowired
     private PasswordHelper passwordHelper;
+/*
+    //  构造函数的调用 是在 注入对象 注入之前，此时我们使用bean会出错
+    //  Constructor >> @Autowired >> @PostConstruct
+    protected UserServiceImpl(){
+        System.out.println("Chenghao--"+userDao.selectUserByName("a"));
+    }
+*/
+
+    @PostConstruct
+    private void init (){
+        //会在userDao Spring容器注入对象 注入之后，自动调用此方法
+        System.out.println("Chenghao-测试@PostConstruct注解-"+userDao.selectUserByName("a"));
+    }
 
     @Override
     public User findUserByName(String name) {
