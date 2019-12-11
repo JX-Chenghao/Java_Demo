b1213a88501e8aa651bf145216c2f11d6d9e8c26
Cheng Hao <hcheng50@best-inc.com>
2019-12-11 10:04:58 +0800

    注册中心eureka,Api网关zuul

---
+++ eureka-server/src/test/java/com/ncu/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncu.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zuul-server/src/test/java/com/ncu/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.ncu.zuulserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+
+}
