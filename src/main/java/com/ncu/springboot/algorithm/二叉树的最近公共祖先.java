a07e494616c3710951b6773180b58e2ee9df25cc
JX-Chenghao <982363676@qq.com>
2024-02-17 19:55:17 +0800

    Initial commit

--- src/main/java/com/ncu/springboot/algorithm/二叉树的最近公共祖先.java
+++ src/main/java/com/ncu/springboot/algorithm/二叉树的最近公共祖先.java
@@ -137,4 +137,38 @@ public class 二叉树的最近公共祖先 {
             return val+"";
         }
     }
+
+
+    /**
+     * 最优解
+     *    划分成左右子树，递归查
+     *    https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/solutions/240096/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestorBest(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            //只要当前根节点是p和q中的任意一个，就返回（因为不能比这个更深了，再深p和q中的一个就没了）
+            return root;
+        }
+        //根节点不是p和q中的任意一个，那么就继续分别往左子树和右子树找p和q
+        TreeNode left = lowestCommonAncestorBest(root.left, p, q);
+        TreeNode right = lowestCommonAncestorBest(root.right, p, q);
+        //p和q都没找到，那就没有
+        if(left == null && right == null) {
+            return null;
+        }
+        //左子树没有p也没有q，就返回右子树的结果
+        if (left == null) {
+            return right;
+        }
+        //右子树没有p也没有q就返回左子树的结果
+        if (right == null) {
+            return left;
+        }
+        //左右子树都找到p和q了，那就说明p和q分别在左右两个子树上，所以此时的最近公共祖先就是root
+        return root;
+    }
 }
